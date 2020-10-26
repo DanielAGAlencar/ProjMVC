60b4e3fae2a9abc4cdca9915778c4c9da0f36b1d
Alencar <alencar@gvmsolucoes.com.br>
2020-10-25 22:05:08 -0300

    Implementação inicial

---
+++ ProjMVC/Controllers/fabricantesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjMVC;
+
+namespace ProjMVC.Controllers
+{
+    public class fabricantesController : Controller
+    {
+        private DadosEntities db = new DadosEntities();
+
+        // GET: fabricantes
+        public ActionResult Index()
+        {
+            return View(db.fabricante.ToList());
+        }
+
+        // GET: fabricantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            fabricante fabricante = db.fabricante.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // GET: fabricantes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: fabricantes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nome,cidade")] fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.fabricante.Add(fabricante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fabricante);
+        }
+
+        // GET: fabricantes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            fabricante fabricante = db.fabricante.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: fabricantes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nome,cidade")] fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fabricante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+        }
+
+        // GET: fabricantes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            fabricante fabricante = db.fabricante.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: fabricantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            fabricante fabricante = db.fabricante.Find(id);
+            db.fabricante.Remove(fabricante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjMVC/Controllers/produtoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjMVC;
+
+namespace ProjMVC.Controllers
+{
+    public class produtoesController : Controller
+    {
+        private DadosEntities db = new DadosEntities();
+
+        // GET: produtoes
+        public ActionResult Index()
+        {
+            var produto = db.produto.Include(p => p.fabricante1);
+            return View(produto.ToList());
+        }
+
+        // GET: produtoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: produtoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.fabricante = new SelectList(db.fabricante, "id", "nome");
+            return View();
+        }
+
+        // POST: produtoes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,descricao,fabricante,preco")] produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.fabricante = new SelectList(db.fabricante, "id", "nome", produto.fabricante);
+            return View(produto);
+        }
+
+        // GET: produtoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.fabricante = new SelectList(db.fabricante, "id", "nome", produto.fabricante);
+            return View(produto);
+        }
+
+        // POST: produtoes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,descricao,fabricante,preco")] produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.fabricante = new SelectList(db.fabricante, "id", "nome", produto.fabricante);
+            return View(produto);
+        }
+
+        // GET: produtoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: produtoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            produto produto = db.produto.Find(id);
+            db.produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
